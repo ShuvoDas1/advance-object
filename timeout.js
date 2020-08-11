5edb90d0278f3c1563032197e9aa03c3c81587ab
ShuvoDas1 <67542645+ShuvoDas1@users.noreply.github.com>
2020-08-11 18:42:13 +0600

    advance object concept

---
+++ timeout.js
@@ -0,0 +1,10 @@
+console.log(1111);
+console.log(2222);
+console.log(3333);
+// setTimeout(() => {
+//     console.log(4444);
+// }, 3000);
+
+setInterval(() =>{
+    console.log('contineu');
+},3000)
